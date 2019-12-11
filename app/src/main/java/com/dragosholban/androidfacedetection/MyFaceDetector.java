bc83c3d3cb7e5951dc7045b910ba296af80c8350
Anshul Tomar <anshultomar@cse.iitb.ac.in>
2019-12-11 16:59:53 +0530

    Added the files
    (cherry picked from commit 36a74da70c4259950a7ed164649bb8fe7115e989)
    
    # Conflicts:
    #	.idea/caches/build_file_checksums.ser
    #	README.md
    #	app/build.gradle
    #	app/src/main/java/com/dragosholban/androidfacedetection/VideoFaceDetectionActivity.java

--- app/src/main/java/com/dragosholban/androidfacedetection/MyFaceDetector.java
+++ app/src/main/java/com/dragosholban/androidfacedetection/MyFaceDetector.java
@@ -18,7 +18,9 @@ import static com.dragosholban.androidfacedetection.FaceDetectionActivity.rotate
 
 class MyFaceDetector extends Detector<Face> {
     private Detector<Face> mDelegate;
-    public ImageView mimv;
+    public ImageView mimv1;
+    public ImageView mimv2;
+    public ImageView mimv3;
     public Frame mFrame;
     public int width,height;
     MyFaceDetector(Detector<Face> delegate) {
@@ -35,16 +37,22 @@ class MyFaceDetector extends Detector<Face> {
         yuvImage.compressToJpeg(new Rect(0, 0, width, height), 100, byteArrayOutputStream);
         byte[] jpegArray = byteArrayOutputStream.toByteArray();
         Bitmap bmp = BitmapFactory.decodeByteArray(jpegArray, 0, jpegArray.length);
-//        SparseArray<Face> detectedFaces = mDelegate.detect(frame);
-//        for(int i=0;i<detectedFaces.size();i++){          //can't use for-each loops for SparseArrays
-//            Face face = detectedFaces.valueAt(i);
-//            //get it's coordinates
-//            Bitmap faceBitmap = Bitmap.createBitmap(bmp, (int)(scaleX(bmp,frame,face.getPosition().x-face.getWidth()/2)),(int)scaleY(bmp,frame,(face.getPosition().y-face.getHeight()/2)),(int)scaleX(bmp,frame,face.getWidth()),(int)scaleY(bmp,frame,face.getHeight()));
-//            //Do whatever you want with this cropped Bitmap
-//
-//        }
-        Bitmap rotatedbitmap = rotateImage(bmp,270);
-        mimv.setImageBitmap(rotatedbitmap);
+        SparseArray<Face> detectedFaces = mDelegate.detect(frame);
+        for(int i=0;i<detectedFaces.size();i++){          //can't use for-each loops for SparseArrays
+            Face face = detectedFaces.valueAt(i);
+            //get it's coordinates
+            Bitmap faceBitmap = Bitmap.createBitmap(bmp, (int)(scaleX(bmp,frame,face.getPosition().x-face.getWidth()/2)),(int)scaleY(bmp,frame,(face.getPosition().y-face.getHeight()/2)),(int)scaleX(bmp,frame,face.getWidth()),(int)scaleY(bmp,frame,face.getHeight()));
+            //Do whatever you want with this cropped Bitmap
+            Bitmap rotatedbitmap = rotateImage(faceBitmap,270);
+            mimv1.setImageBitmap(rotatedbitmap);
+            Bitmap lefteyeBitmap = Bitmap.createBitmap(bmp, (int)(scaleX(bmp,frame,face.getPosition().x+3*face.getWidth()/10)),(int)scaleY(bmp,frame,face.getPosition().y+3*face.getHeight()/8),40,40);
+            Bitmap righteyeBitmap = Bitmap.createBitmap(bmp,(int)(scaleX(bmp,frame,face.getPosition().x+7*face.getWidth()/10)),(int)scaleY(bmp,frame,face.getPosition().y+3*face.getHeight()/8),40,40);
+            Bitmap rleb = rotateImage(lefteyeBitmap,270);
+            Bitmap rreb = rotateImage(righteyeBitmap,270);
+            mimv2.setImageBitmap(rleb);
+            mimv3.setImageBitmap(rreb);
+        }
+
         return mDelegate.detect(frame);
     }
 
@@ -55,8 +63,8 @@ class MyFaceDetector extends Detector<Face> {
     public boolean isOperational() {
         return mDelegate.isOperational();
     }
-    public void setimageview(ImageView imv){
-        mimv = imv;
+    public void setimageview(ImageView imv1,ImageView imv2,ImageView imv3){
+        mimv1 = imv1;mimv2 = imv2; mimv3 = imv3;
     }
     public boolean setFocus(int id) {
         return mDelegate.setFocus(id);
